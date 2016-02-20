87bf219caab2f1912aa50b44be292b430bbd5fee
sharhar <wiishahar@gmail.com>
2016-02-20 12:50:12 -0800

    finished basic game!

--- client.cpp
+++ client.cpp
@@ -16,8 +16,12 @@ template < typename T > std::string to_string( const T& n ) {
 sf::TcpSocket socket;
 bool running = true;
 int y = 100;
-int player1Y = 0;
-int player2Y = 0;
+int player1Y = 100;
+int player2Y = 100;
+int ballX = 300;
+int ballY = 300;
+int p1Score = 0;
+int p2Score = 0;
 int num = 0;
 bool send = false;
 
@@ -30,13 +34,23 @@ void game() {
 	sf::RenderWindow window(sf::VideoMode(640, 480), ("Player " + to_string(num)), sf::Style::Default, sf::ContextSettings());
 	window.setVerticalSyncEnabled(true);
 
-	glClearColor(0.2f, 0.2f, 0.2f, 1);
+	glClearColor(0.1f, 0.1f, 0.1f, 1);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	glOrtho(0, 640, 0, 480, -1, 1);
 	glMatrixMode(GL_MODELVIEW);
 
 	bool focused = false;
+
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf")) {
+		return;		
+	}
+
+	sf::Text text;
+	text.setFont(font);
+	text.setCharacterSize(24);
+	text.setPosition(300, 30);
 
 	while (running) {
 
@@ -58,15 +72,20 @@ void game() {
 
         glBegin(GL_QUADS);
         {
-        	glVertex2f(100, player1Y);
+        	glVertex2f(100, player1Y    );
         	glVertex2f(100, player1Y+100);
         	glVertex2f(115, player1Y+100);
-        	glVertex2f(115, player1Y);
+        	glVertex2f(115, player1Y    );
 
-        	glVertex2f(500, player2Y);
+        	glVertex2f(500, player2Y    );
         	glVertex2f(500, player2Y+100);
         	glVertex2f(515, player2Y+100);
-        	glVertex2f(515, player2Y);
+        	glVertex2f(515, player2Y    );
+
+			glVertex2f(ballX     , ballY   );
+        	glVertex2f(ballX     , ballY+20);
+        	glVertex2f(ballX + 20, ballY+20);
+        	glVertex2f(ballX + 20, ballY   );
         }
         glEnd();
 
@@ -83,6 +102,13 @@ void game() {
 	        }
         }
 
+
+		text.setString(to_string(p1Score) + " | " + to_string(p2Score));
+		
+		window.pushGLStates();
+        window.draw(text);
+		window.popGLStates();
+        
         window.display();
     }
 }
@@ -103,52 +129,65 @@ void listen() {
 			int number = StringUtils::parseInt(parts[1]);
 			std::cout << "Number is: " << number << std::endl;
 			num = number;
-		} else if(parts[0].compare(std::string("P")) == 0) {
+		} else if(parts[0].compare(std::string("U")) == 0) {
 			int number1 = StringUtils::parseInt(parts[1]);
 			int number2 = StringUtils::parseInt(parts[2]);
+			int number3 = StringUtils::parseInt(parts[3]);
+			int number4 = StringUtils::parseInt(parts[4]);
+			int number5 = StringUtils::parseInt(parts[5]);
+			int number6 = StringUtils::parseInt(parts[6]);
 			player1Y = number1;
 			player2Y = number2;
+			ballX    = number3;
+			ballY    = number4;
+			p1Score  = number5;
+			p2Score  = number6;
 		}
 	}
 }
 
 int main() {
-	sf::Socket::Status status = socket.connect("localhost", 25565);
-	
-	if (status != sf::Socket::Done) {
-    	std::cout << "Could not connect to server!" << std::endl;
-    	return -1;
-    }
-
-	sf::Thread lis(&listen);
 	sf::Thread loop(&game);
-	
-	char data[100];
-	std::size_t received;
-	
-	if (socket.receive(data, 100, received) != sf::Socket::Done) {
-		running = false;
-   		std::cout << "Could not recive data!" << std::endl;
-	}
 
-	std::string str(data);
-	std::vector<std::string> parts = StringUtils::split(str, ' ');
-	
-	int number = StringUtils::parseInt(parts[1]);
-	std::cout << "Number is: " << number << std::endl;
-	num = number;
+	// std::cout << "Enter port: ";
+	// int port = 0;
+	// std::cin >> port;
 
-	lis.launch();
+	// sf::Socket::Status status = socket.connect("localhost", port);
+	
+	// if (status != sf::Socket::Done) {
+ //    	std::cout << "Could not connect to server!" << std::endl;
+ //    	return -1;
+ //    }
+
+	// sf::Thread lis(&listen);
+	
+	// char data[100];
+	// std::size_t received;
+	
+	// if (socket.receive(data, 100, received) != sf::Socket::Done) {
+	// 	running = false;
+ //   		std::cout << "Could not recive data!" << std::endl;
+	// }
+
+	// std::string str(data);
+	// std::vector<std::string> parts = StringUtils::split(str, ' ');
+	
+	// int number = StringUtils::parseInt(parts[1]);
+	// std::cout << "Number is: " << number << std::endl;
+	// num = number;
+
+	// lis.launch();
 	loop.launch();
 
 	while(running) {
-		if(send) {
-			std::string message = "P " + to_string(y);
+		// if(send) {
+		// 	std::string message = "P " + to_string(y);
 
-			if (socket.send(message.c_str(), 100) != sf::Socket::Done) {
+		// 	if (socket.send(message.c_str(), 100) != sf::Socket::Done) {
 
-			}
-		}
+		// 	}
+		// }
 
 		sleep(0.02f);
 	}
--- server.cpp
+++ server.cpp
@@ -12,12 +12,31 @@ template < typename T > std::string to_string( const T& n ) {
 bool running = true;
 sf::TcpSocket clientSocket1;
 sf::TcpSocket clientSocket2;
+sf::Clock CC;
 
-int p1y = 0;
-int p2y = 0;
+int p1y = 100;
+int p2y = 100;
+int ballX = 300;
+int ballY = 300;
+int p1s = 3;
+int p2s = 3;
+
+int bsx = 2;
+int bsy = 2;
 
 void sleep(float sec) {
 	sf::sleep(sf::seconds(sec));
+}
+
+bool isHitting(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+	if(x1 + w1 < x2 || x2 + w2 < x1) {
+		return false;
+	}
+	if(y1 + h1 < y2 || y2 + h2 < y1) {
+		return false;
+	}
+
+	return true;
 }
 
 void client1() {
@@ -57,11 +76,60 @@ void client2() {
 	}
 }
 
+void game() {
+	CC.restart();
+
+	bool started = false;
+
+	std::cout << "String game loop!\n";
+
+	while(running) {
+		if(!started) {
+			if(CC.getElapsedTime().asSeconds() > 3.0f) {
+				started = true;
+				p1s = 0;
+				p2s = 0;
+			}else if(CC.getElapsedTime().asSeconds() > 2.0f) {
+				p1s = 1;
+				p2s = 1;
+			}else if(CC.getElapsedTime().asSeconds() > 1.0f) {
+				p1s = 2;
+				p2s = 2;
+			}
+		} else {
+			ballX += bsx;
+			ballY += bsy;
+
+			if(ballY+20 > 480 || ballY < 0) {
+				bsy = -bsy;
+			}
+			if(isHitting(100, p1y, 15, 100, ballX, ballY, 20, 20) || isHitting(500, p2y, 15, 100, ballX, ballY, 20, 20)) {
+				bsx = -bsx;
+			}
+
+			if(ballX < 0) {
+				p1s++;
+				ballX = 300;
+			}
+			if(ballX+20 > 640) {
+				p2s++;
+				ballX = 300;
+			}
+		}
+
+		sleep(0.02f);
+	}
+}
+
 int main() {
 	sf::TcpListener listener;
 
-	if(listener.listen(25565) != sf::Socket::Done) {
-		std::cout << "Error when trying to bind to port 25565!" << std::endl;
+	std::cout << "Enter port: ";
+	int port = 0;
+	std::cin >> port;
+
+	if(listener.listen(port) != sf::Socket::Done) {
+		std::cout << "Error when trying to bind to port " << port << "!" << std::endl;
     	return -1;
 	}
 
@@ -91,18 +159,23 @@ int main() {
 
 	std::cout << "Player 2 connected!" << std::endl;
 
+	sf::Thread loop(&game);
+
 	thread2.launch();
 	thread1.launch();
+	loop.launch();
 
 	std::cout << "starting main game loop!" << std::endl;
 
 	while(running) {
 		//std::cout << "p1 " << p1y << "\tp2 " << p2y << std::endl;
+		std::string message = "U " + to_string(p1y) + " " + to_string(p2y) + " " + to_string(ballX) + " " + to_string(ballY) + " " + to_string(p1s) + " " + to_string(p2s);
+		const char* cMessage = message.c_str();
 
-		if(clientSocket1.send(("P " + to_string(p1y) + " " + to_string(p2y)).c_str(), 100) != sf::Socket::Done) {
+		if(clientSocket1.send(cMessage, 100) != sf::Socket::Done) {
 			std::cout << "Could not send data!" << std::endl;
 		}
-		if(clientSocket2.send(("P " + to_string(p1y) + " " + to_string(p2y)).c_str(), 100) != sf::Socket::Done) {
+		if(clientSocket2.send(cMessage, 100) != sf::Socket::Done) {
 			std::cout << "Could not send data!" << std::endl;
 		}
 
